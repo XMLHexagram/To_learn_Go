116ca0a20295f27437b0beb3874135b40db74042
Hexagram <1471685806@qq.com>
2019-10-22 14:23:42 +0800

    fine

--- try_tiqi/try.go
+++ try_tiqi/try.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	//"fmt"
-	"io"
-	"net/http"
-	"os"
+	//"io"
+	//"net/http"
+	//"os"
+	"fmt"
+)
+
+const (
+	sunday = iota
+	monday
+	tuseday
+	wednesday
+	thursday
+	friday
+	saturday
 )
 
 func main() {
-	resp, error := http.Get("https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json ")
-	if error != nil {
-		//deal with error
-		return
+	fmt.Println(sunday)
+	fmt.Println(saturday)
+
+	number := [6]int{1, 2, 3, 4, 5} //没赋值默认是0
+	for i, x := range number {
+		fmt.Printf("第%d位的值 = %d\n", i, x)
 	}
-	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+	//resp, error := http.Get("https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json ")
+	//if error != nil {
+	//deal with error
+	//	return
+	//}
+	//defer resp.Body.Close()
+	//io.Copy(os.Stdout, resp.Body)
 }
