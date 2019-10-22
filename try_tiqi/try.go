c9fd97e0c2a816dad0caf4dbc8525be899b6b8ce
Hexagram <1471685806@qq.com>
2019-10-22 13:16:10 +0800

    fine

---
+++ try_tiqi/try.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	//"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, error := http.Get("https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json ")
+	if error != nil {
+		//deal with error
+		return
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+}
