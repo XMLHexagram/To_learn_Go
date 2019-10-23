a9eb0487eddbf3104cdeb88c4501a119209db5b2
lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>
2019-10-23 12:19:46 +0800

    fine

---
+++ try_tiqi/try3_api/try3.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json")
+	checkError(err)
+	data, err := ioutil.ReadAll(res.Body)
+	checkError(err)
+	fmt.Printf("Got: %q", string(data))
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalf("Get : %v", err)
+	}
+}
