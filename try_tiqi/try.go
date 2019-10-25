093e6c83e9b137025fa6eb145b02644283b93615
lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>
2019-10-25 11:25:04 +0800

    fine

--- try_tiqi/try.go
+++ try_tiqi/try.go
@@ -1,52 +1,28 @@
 package main
 
 import (
-	//"fmt"
-	//"io"
-	//"net/http"
-	//"os"
 	"fmt"
-)
-
-const (
-	sunday = iota
-	monday
-	tuseday
-	wednesday
-	thursday
-	friday
-	saturday
+	"time"
 )
 
 func main() {
-	a := 100
-	var b int = 200
-
-	fmt.Println(sunday)
-	fmt.Println(saturday)
-
-	number := [6]int{1, 2, 3, 4, 5} //没赋值默认是0
-	for i, x := range number {
-		fmt.Printf("第%d位的值 = %d\n", i, x)
-	}
-
-	println(max(a, b))
-	//resp, error := http.Get("https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json ")
-	//if error != nil {
-	//deal with error
-	//	return
-	//}
-	//defer resp.Body.Close()
-	//io.Copy(os.Stdout, resp.Body)
+	fmt.Println("In main()")
+	go longWait()
+	go shortWait()
+	fmt.Println("About to sleep in main()")
+	// sleep works with a Duration in nanoseconds (ns) !
+	time.Sleep(10 * 1e9)
+	fmt.Println("At the end of main()")
 }
 
-func max(num1, num2 int) int {
-	var result int
+func longWait() {
+	fmt.Println("Beginning longWait()")
+	time.Sleep(5 * 1e9) // sleep for 5 seconds
+	fmt.Println("End of longWait()")
+}
 
-	if num1 > num2 {
-		result = num1
-	} else {
-		result = num2
-	}
-	return result
+func shortWait() {
+	fmt.Println("Beginning shortWait()")
+	time.Sleep(2 * 1e9) // sleep for 2 seconds
+	fmt.Println("End of shortWait()")
 }
--- wether_API/fisrt.go
+++ wether_API/fisrt.go
@@ -6,28 +6,93 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"./server"
 )
 
-type weather struct {
-	status      string
-	lang        string
-	api_version string
+func main() {
+	go server.MainOut()
+	go jieXiURL()
 }
 
-func main() {
-	fmt.Println(b)
+func jieXiURL() {
 	var temp weather
+	url := "https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json"
+	json.Unmarshal(getJS(url), &temp)
+	fmt.Println(temp)
+	fmt.Printf("\n\n")
+	temp.printWeather()
+}
 
-	res, err := http.Get("https://api.caiyunapp.com/v2/TAkhjf8d1nlSlspN/121.6544,25.1552/realtime.json")
+func (w weather) printWeather() {
+	fmt.Println(w.Status)
+	fmt.Println(w.Lang)
+	fmt.Println(w.Unit)
+}
+
+func getJS(URL string) []byte {
+	res, err := http.Get(URL)
 	checkError(err)
 	data, err := ioutil.ReadAll((res.Body))
 	checkError(err)
-	json.Unmarshal(data, &temp)
-	fmt.Println(temp)
+	return data
 }
 
 func checkError(err error) {
 	if err != nil {
 		log.Fatalf("Get : %v", err)
 	}
+}
+
+type weather struct {
+	Status     string    `json:"status"`
+	Lang       string    `json:"lang"`
+	Unit       string    `json:"unit"`
+	ServerTime int       `json:"server_time"`
+	Location   []float64 `json:"location"`
+	APIStatus  string    `json:"api_status"`
+	Tzshift    int       `json:"tzshift"`
+	APIVersion string    `json:"api_version"`
+	Result     struct {
+		Status        string  `json:"status"`
+		O3            float64 `json:"o3"`
+		Co            float64 `json:"co"`
+		Temperature   float64 `json:"temperature"`
+		Pm10          int     `json:"pm10"`
+		Skycon        string  `json:"skycon"`
+		Cloudrate     float64 `json:"cloudrate"`
+		Precipitation struct {
+			Nearest struct {
+				Status    string  `json:"status"`
+				Distance  float64 `json:"distance"`
+				Intensity float64 `json:"intensity"`
+			} `json:"nearest"`
+			Local struct {
+				Status     string `json:"status"`
+				Intensity  int    `json:"intensity"`
+				Datasource string `json:"datasource"`
+			} `json:"local"`
+		} `json:"precipitation"`
+		Dswrf       float64 `json:"dswrf"`
+		Visibility  int     `json:"visibility"`
+		Humidity    float64 `json:"humidity"`
+		So2         float64 `json:"so2"`
+		Ultraviolet struct {
+			Index int    `json:"index"`
+			Desc  string `json:"desc"`
+		} `json:"ultraviolet"`
+		Pres                float64 `json:"pres"`
+		Aqi                 int     `json:"aqi"`
+		Pm25                int     `json:"pm25"`
+		No2                 float64 `json:"no2"`
+		ApparentTemperature float64 `json:"apparent_temperature"`
+		Comfort             struct {
+			Index int    `json:"index"`
+			Desc  string `json:"desc"`
+		} `json:"comfort"`
+		Wind struct {
+			Direction int     `json:"direction"`
+			Speed     float64 `json:"speed"`
+		} `json:"wind"`
+	} `json:"result"`
 }
