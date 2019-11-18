9edb45988d775e0712a7d7efdfe3b4f4d364564f
Hexagram <1471685806@qq.com>
2019-11-18 22:53:55 +0800

    fine

--- try7/main.go
+++ try7/main.go
@@ -5,8 +5,21 @@ import (
 	"runtime"
 )
 
+func sum10000(i chan struct{}) {
+	var sum int = 0
+	for i := 0; i <= 10000; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+	i <- struct{}{}
+}
+
 func main() {
+	C := make(chan struct{})
 	fmt.Println(runtime.GOMAXPROCS(0))
-	runtime.GOMAXPROCS(5)
-	fmt.Println(runtime.GOMAXPROCS(0))
+	go sum10000(C)
+
+	fmt.Printf("test go\n")
+	<-C
+	//time.Sleep(5 * time.Second)
 }
